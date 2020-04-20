43e7ab21757c166e4eb3f2ed4e79403f33a1ed31
HOUAYS Yassine <Yass76300@gmail.com>
2020-04-20 17:09:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 3000; 
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+routes(app);
+
+mongoose.connect('mongodb+srv://test:root@cours-zyx04.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
+  .then(result => {
+    app.listen(port);
+    console.log('Your first node api is running on port: ' + port);
+  })
+  .catch(err => console.log(err));
+
+ 
+
+module.exports = app;
